388b3d49412d7d63dba28c142600012a535125c3
Bruno Sales <30627720+BrunoSalesCI@users.noreply.github.com>
2018-11-01 18:02:32 -0300

    UPDATE IV
    GPIO inserted :)

--- washingMachine.v
+++ washingMachine.v
@@ -1,245 +1,203 @@
-module washingMachine(clk,reset,moeda, lid_r, d_lavar, Tempo, molho, enxague, centrifugar, lavar, pausar, parada, botao_50cent, botao_1real);
- input clk,reset,moeda,lid_r,d_lavar,Tempo;
- output reg molho,enxague,centrifugar,lavar,pausar,parada;
- reg [2:0] count;
- 
- reg[2:0] estado_atual, proximo_estado;
- parameter ESPERA = 3'b000,
-           MOLHO = 3'b001,
-           LAVAR = 3'b010,
-		   ENXAGUE = 3'b011,
-           LAVAR2 = 3'b100,
-           ENXAGUE2 = 3'b101,
-           CENTRIFUGAR = 3'b110,
-           PAUSAR = 3'b111;
-           
+module washing_Machine(
+		input CLOCK_27,  //Clock
+		output reg [3:0] LEDG,		//bomba_agua = 1000, modo_agitar = 0100, modo_centrifugar = 0001, modo_girar = 0010
+		output reg [6:0] HEX6,
+		output reg [6:0] HEX4,
+		output reg [6:0] HEX7,
+		output reg [6:0] HEX5,
+		output reg [6:0] HEX3,
+		output reg [6:0] HEX2,
+		output reg [6:0] HEX0,
+		output reg GPIO,
+		input wire [1:0] SW, //controle de modos
+		output reg [17:0] LEDR);  //usado para teste e visualizaÃ§Ã£o do contador que controla
+																	  //o contador que muda de estado
+
+	initial HEX6 = 7'b1111111;
+	initial HEX4 = 7'b1111111;
+	integer contador = 0;	//contador que gerencia os estados
+	integer jaContou;	//variavel que assegura que o contador nÃ£o incrementa varias vezes por conta da velocidade do clock
+	reg flag = 0;		//flag para zerar o contador
+	
+	reg [23:0] cnt;									//cnt Ã© um contador que funciona como prescaler
+	always @(posedge CLOCK_27) cnt <= cnt+24'h1;
+	wire cntovf = &cnt;
+
+						//Dando Set no contador baseado no clock interno
+	reg [3:0] UNI;		//UNI eh um contador que conta de 0 a 9
+	always @(posedge CLOCK_27) begin		
+		if(cntovf) UNI = (UNI==4'h9 ? 4'h0 : UNI+4'h1);	//incrementa o uni ou zera se seu valor for 9
+	end													//essa variaÃ§Ã£o acontece num tempo prÃ³ximo de 1 segundo
+	
 
 
-
-/*always @(posedge botao_50cent or posedge botao_1real or negedge reset)
-begin
-	if(~reset)
-		count <= 0;
-	else if(botao_50cent == 1)
-		count = count + 1;
-	if(botao_1real == 1)
-		count = count + 2;
-end
-*/
-
-// ATRIBUIÇÃO DE ESTADO - PARTE COMBINACIONAL
-
-always @(estado_atual or moeda or d_lavar or lid_r or Tempo)
-begin
-   case (estado_atual)
-    ESPERA:
-	if(moeda==1)
-         begin
-         proximo_estado=MOLHO;
-         molho=1;
-         enxague=0;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-else
-
-         begin
-         proximo_estado=estado_atual;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-
-MOLHO:
-if(Tempo==1)													//T = Espera pelo tempo
-
-         begin
-         proximo_estado=LAVAR;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=1;
-         pausar=0;
-         parada=0;
-         end
-else
-         begin
-         proximo_estado=estado_atual;
-         molho=1;
-         enxague=0;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-          end
-LAVAR:
-if(Tempo==1)
-
-         begin
-         proximo_estado=ENXAGUE;
-         molho=0;
-         enxague=1;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-else
-         begin
-         proximo_estado=estado_atual;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=1;
-         pausar=0;
-         parada=0;
-         end
-ENXAGUE2:
-if(Tempo==1 && d_lavar==1)
-
-         begin
-         proximo_estado=LAVAR2;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=1;
-         pausar=0;
-         parada=0;
-         end
-else if(Tempo==1 && d_lavar==0)
-
-         begin
-         proximo_estado=CENTRIFUGAR;
-         molho=0;
-         enxague=0;
-         centrifugar=1;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-else
-         begin
-         proximo_estado=estado_atual;
-         molho=0;
-         enxague=1;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-LAVAR2:
-if(Tempo==1)
-
-         begin
-         proximo_estado=ENXAGUE2;
-         molho=0;
-         enxague=1;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-else
-         begin
-         proximo_estado=estado_atual;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=1;
-         pausar=0;
-         parada=0;
-         end
-
-ENXAGUE:
-if(Tempo==1)
-         begin
-         proximo_estado=CENTRIFUGAR;
-         molho=0;
-         enxague=0;
-         centrifugar=1;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-else
-         begin
-         proximo_estado=estado_atual;
-         molho=0;
-         enxague=1;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-CENTRIFUGAR:
-if(Tempo==0 && lid_r==1)
-         begin
-         proximo_estado=PAUSAR;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=0;
-         pausar=1;
-         parada=0;
-         end
-else if(Tempo==1)
-         begin
-         proximo_estado=ESPERA;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-else
-         begin
-         proximo_estado=estado_atual;
-         molho=0;
-         enxague=0;
-         centrifugar=1;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-PAUSAR:
-if(lid_r==0)
-         begin
-         proximo_estado=CENTRIFUGAR;
-         molho=0;
-         enxague=0;
-         centrifugar=1;
-         lavar=0;
-         pausar=0;
-         parada=0;
-         end
-else
-         begin
-         proximo_estado=estado_atual;
-         molho=0;
-         enxague=0;
-         centrifugar=0;
-         lavar=0;
-         pausar=1;
-         parada=0;
-         end
-		 default: proximo_estado=ESPERA;
-		 endcase
+	reg [1:0] modo; 	//variavel que guarda o modo da maquina de lavar
+	
+	parameter modo_espera = 0, limpeza_padrao = 1, limpeza_rapida = 2;	 //modos
+	
+	always @ (posedge CLOCK_27) begin	//selecao de modo
+		case (SW[1:0])
+			2'b00:
+			begin
+				HEX3 = 7'b1000110;
+				HEX2 = 7'b1000111;								//CL2 NO DISPLAY
+				HEX0 = 7'b0100100;
+				HEX7 = 7'b0010010;								//SIMPLES (S) NO DISPLAY
+				HEX5 = 7'b1000110;								//COMPLETO (C) NO DISPLAY
+				LEDR = 18'b000000000000000000;
+				modo <= modo_espera;	//maquina em espera
+			end
+			2'b01: 
+			begin
+				HEX3 = 7'b1000110;
+				HEX2 = 7'b1000111;								//CL2 NO DISPLAY
+				HEX0 = 7'b0100100;
+				HEX7 = 7'b0010010;
+				HEX5 = 7'b1000110;
+				modo <= limpeza_padrao;		//ciclo completo
+			end
+			2'b10: 
+			begin
+				HEX3 = 7'b1000110;
+				HEX2 = 7'b1000111;								//CL2 NO DISPLAY
+				HEX0 = 7'b0100100;
+				HEX7 = 7'b0010010;
+				HEX5 = 7'b1000110;
+				modo <= limpeza_rapida;		//ciclo simplificado
+			end
+			2'b11:						//caso de erro
+			begin
+				HEX3 = 7'b1000110;
+				HEX2 = 7'b1000111;								//CL2 NO DISPLAY
+				HEX0 = 7'b0100100;
+				HEX7 = 7'b1111111;
+				HEX5 = 7'b1111111;
+				LEDR = 18'b111111111111111111;	//sinal de erro que indica que os dois modos nÃo podem estar ativos ao mesmo tempo
+				modo <= modo_espera;
+			end
+		endcase
 	end
+	
+	initial GPIO = 1'b0;
+	
+	always@ (posedge CLOCK_27) begin //controle do contador que gerencia os estados
+		if(UNI == 4'h0)				//zera o jaContou quando UNI chega em 0 para o contador ir para a prÃ³xima etapa
+			jaContou = 0;
 
+		if (UNI == 4'h9 && ((SW == 2'b01) || (SW == 2'b10)) && jaContou == 0) begin		//incrementa o contador
+			contador = contador + 1;
+			jaContou = 1;
+		end
 
-always @(posedge clk or negedge reset)			// ATRIBUIÇÃO DE FUNÇÃO - PARTE SEQUENCIAL
-begin
-   if (~reset)
-     estado_atual <= ESPERA;
-    else
-     estado_atual <= proximo_estado;
- end
- 
+		if (SW == 2'b00)	//se os dois switchs estiverem desligados, o contador que gerencia os estados nÃ£o incrementa
+			contador = 0;
 
+	end
+	
+	
+	always @ (posedge CLOCK_27) begin 	//funcionalidade
+		case (modo)
+			modo_espera:
+			begin
+				HEX4 = 7'b1111111;
+				HEX6 = 7'b1111111; 	// PARTE PARA TESTAR
+				LEDG = 4'b0000;
+			end
+							//contador:
+			limpeza_padrao:	//espera = 0, encher = 1, agitar = 2, tempo = 3, agitar2 = 4, esvaziar = 5, centrifugar = 6, fim = 7
+			begin
+				//espera >> encher
+				if(contador == 1) begin											//ENCHER
+					GPIO <= 1'b0;
+					HEX4 = 7'b0000110;
+					LEDG = 4'b1000;
+				end
+				
+				//enchendo >> modo agitar
+				if (contador == 2) begin										//AGITAR
+					GPIO <= 1'b1;
+					HEX4 = 7'b0001000;
+					LEDG = 4'b0100;	
+				end
+
+				//agitar >> tempo
+				if (contador == 3) begin										//PAUSA, MOLHO
+					GPIO <= 1'b0;
+					HEX4 = 7'b0001100;
+					LEDG = 4'b0010;
+				end
+
+				//tempo >> agitar2
+				if (contador == 4) begin										//AGITAR 2
+					GPIO <= 1'b1;
+					HEX4 = 7'b0001000;
+					LEDG = 4'b0100;
+				
+				end
+
+				//agitar2 >> esvaziar
+				if (contador == 5) begin
+					GPIO <= 1'b0;
+					HEX4 = 7'b0100001;											//DRENAR
+					LEDG = 4'b0000;
+				end
+
+				//esvaziar >> centrifugar
+				if (contador == 6) begin
+					GPIO <= 1'b1;
+					HEX4 = 7'b1000110;											//CENTRIFUGAR
+					LEDG = 4'b0001;
+				end
+
+				//centrifugar >> fim
+				if (contador == 7) begin
+					GPIO <= 1'b0;
+					HEX4 = 7'b0001110;											//FIM		
+					LEDG = 4'b1111;
+				end
+
+			end
+							//contador:
+			limpeza_rapida:	//espera = 0, encher = 1, agitar = 2, esvaziar = 3, centrifugar = 4, fim = 5
+
+				
+				begin	
+					//espera >> encher
+					if(contador == 1) begin
+						GPIO <= 1'b0;
+						HEX6 = 7'b0000110;										//ENCHER
+						LEDG = 4'b1000;
+					end
+
+					//enchendo >> modo agitar
+					if (contador == 2) begin
+						GPIO <= 1'b1;
+						HEX6 = 7'b0001000;										//AGITAR
+						LEDG = 4'b0100;
+					end
+
+					//agitar >> esvaziar
+					if (contador == 3) begin
+						GPIO <= 1'b0;
+						HEX6 = 7'b0100001;									//DRENAR
+						LEDG = 4'b0000;
+					end
+					
+					//esvaziar >> centrifugar
+					if (contador == 4) begin
+						GPIO <= 1'b1;
+						HEX6 = 7'b1000110;										//CENTRIFUGAR
+						LEDG = 4'b0001;
+					end
+
+					//centrifugar >> fim
+					if (contador == 5) begin
+						GPIO <= 1'b0;
+						HEX6 = 7'b0001110;										//FIM
+						LEDG = 4'b1111;
+					end
+
+				end
+		endcase
+	end
 endmodule
